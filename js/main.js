7f66d845583e35d080b08f8b4ea7eb1b0ed7a047
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-02-23 20:51:20 +0000

    Add event listener for dive containing 3 buttons. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 var $pressHereButton = document.querySelector('.press-here');
 var $modal = document.querySelector('.modal-background');
 var $appName = document.querySelector('.app-name');
+var $buttonContainer = document.querySelector('.button-container');
+
 $pressHereButton.addEventListener('click', function (event) {
   $modal.className = 'modal-background';
   $pressHereButton.textContent = '';
   $appName.className = 'hidden app-name';
+});
+
+$buttonContainer.addEventListener('click', function (event) {
+  // console.log(event.target.tagName);
 });
 
 // var xhr = new XMLHttpRequest();
