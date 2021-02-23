de5796338497b863ed189ffe72312d1e7e38879c
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-02-23 19:42:50 +0000

    Added event listener to Press Here button to open modal. #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var $pressHereButton = document.querySelector('.press-here');
+var $modal = document.querySelector('.modal-background');
+$pressHereButton.addEventListener('click', function (event) {
+  $modal.className = 'modal-background';
+});
 
 // var xhr = new XMLHttpRequest();
 // xhr.open('GET', 'https://pokeapi.co/api/v2/pokedex/1/');
