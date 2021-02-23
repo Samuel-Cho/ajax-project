25d973463eeb66cc109daab959aa007e456153dc
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-02-23 04:36:22 +0000

    Started html and css files for homepage. #1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://pokeapi.co/api/v2/pokedex/1/');
+// xhr.responseType = 'json';
+// xhr.addEventListener('load', function() {
+//   console.log('status:', xhr.status);
+//   console.log('response:', xhr.response);
+// });
+// xhr.send();
